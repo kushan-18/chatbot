eeda6ecf581a8fab2b68edd693bff5b33c8ebfbb
Kushan Dholiya <kushandholiya@gmail.com>
2026-09-25 18:57:54 +0530

    Update suggestions array with new prompts

--- lib/constants.ts
+++ lib/constants.ts
@@ -13,8 +13,8 @@ export const guestRegex = /^guest-\d+$/;
 export const DUMMY_PASSWORD = generateDummyPassword();
 
 export const suggestions = [
-  "What are the advantages of using Next.js?",
-  "Write code to demonstrate Dijkstra's algorithm",
-  "Help me write an essay about Silicon Valley",
-  "What is the weather in San Francisco?",
+  "Explain a tricky DSA concept with an example",
+  "Help me debug this piece of code",
+  "Write an email or message for me",
+  "Summarize this text for me",
 ];
